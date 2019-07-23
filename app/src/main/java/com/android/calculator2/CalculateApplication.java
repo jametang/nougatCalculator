c80153fd4de8bce6079c0128a1fa332911b4d7eb
tangli <18782994956@163.com>
2019-07-23 23:51:37 +0800

    集成tencent mat sdk

---
+++ app/src/main/java/com/android/calculator2/CalculateApplication.java
@@ -0,0 +1,17 @@
+package com.android.calculator2;
+
+import android.app.Application;
+
+import com.tencent.stat.StatConfig;
+import com.tencent.stat.StatService;
+
+public class CalculateApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        StatConfig.setDebugEnable(true);
+        StatService.registerActivityLifecycleCallbacks(this);
+
+    }
+}
